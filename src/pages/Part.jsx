5ea2d65c7ba3964a7e52adc14847bac55d88d10c
Sylvain Crouzier <sylvain.crouzier@free.fr>
2026-09-14 11:52:03 +0200

    change 10

--- src/pages/Part.jsx
+++ src/pages/Part.jsx
@@ -6,7 +6,7 @@ function Part() {
             <div className="partCont">
                 <h2 className="partTitle">Run'Illac remercie son généreux partenaire</h2>
                 <p className="partId">M. Philippe Verdier, et la société Diro-Atlantique</p>
-                <img className="partImg" src="/img/logo-diro.png" alt="diro" />
+                <img className="partImg" src="./img/logo-diro.png" alt="diro" />
                 {/* <p className="partId">M. Christophe Huard, et le groupe Volkswagen</p> */}
                 {/* <img className="partImg" src="/img/volkswagen.webp" alt="vw" /> */}
             </div>
